e4d79f7ee1b4923d0a2312facbf0406243bae987
ocean998 <40862700+ocean998@users.noreply.github.com>
2019-09-24 22:45:59 +0800

    打印货架信息

--- 3_立库模拟_测试.py
+++ 3_立库模拟_测试.py
@@ -1,41 +1,33 @@
 from pandas import DataFrame
 import pandas as pd
 import numpy as np
+import queue
 
-df = DataFrame( columns=['A', 'B', 'C'])
-ndf = DataFrame([[1,2,3]], columns=['A', 'B', 'C'])
-print(ndf)
-print('==============')
-df = df.append(ndf, ignore_index=True)
-pd.concat([df, ndf], axis=0)
-ndf = DataFrame([[4,5,6]],columns=['A', 'B', 'C'])
-df = df.append(ndf, ignore_index=True)
-print(df)
 
-print('=======df 添加行完成=======')
 
-df = DataFrame(np.random.randn(4, 5), columns=['A', 'B', 'C', 'D', 'E'])
-print(df)
 
-# 计算各列数据总和并作为新列添加到末尾
-df['Col_sum'] = df.apply(lambda x: x.sum(), axis=1)
-print(df)
+# 总任务数量平均分，有余数的最后一个子任务调节
+def div_task(total:int = 0, cnt:int = 8):
+    every_cnt = int( round(total/cnt) )
+    x = total%cnt
+    tl = []
+    if x > 0:
+        for i in range(cnt-1):
+            tl.append(every_cnt)
+        tl.append(total- every_cnt*(cnt-1))
+    else:
+        for i in range(cnt):
+            tl.append(every_cnt)
+    rst = np.array(tl)
+    print(rst,rst.sum())
 
-row_sum = df.apply(lambda x: x.sum())
-print(row_sum['B'])
-print(row_sum[['A','B']])
-print(type( row_sum ) )
+if __name__ == '__main__':
+    dd = np.random.randint( 50, 300, 10 ) * 100
 
-df1 = df.sum(axis=0)
-print(df1)
-print('==============')
-print(df1['B'])
-
-# 计算各行数据总和并作为新行添加到末尾
-df.loc['Row_sum'] = df.apply(lambda x: x.sum())
-# print(df)
-
-print('==============')
-print(df['B'])
-
-print("空储位{}, 入库数量{}，不能入库！".format(100, 110))
+    qt = queue.Queue()
+    for cnt in dd:
+        qt.put( cnt )
+    print( dd )
+    while not qt.empty():
+        xx = qt.get()
+        div_task( xx , 8)
